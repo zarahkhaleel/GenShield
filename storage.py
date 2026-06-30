20ea51413403ca51630680f880e3538a08896fa9
zarahkhaleel <167708035+zarahkhaleel@users.noreply.github.com>
2026-06-30 12:30:36 +0100

    Add GenLayer smart contract (storage.py)

---
+++ storage.py
@@ -0,0 +1,23 @@
+# v0.2.16
+# { "Depends": "py-genlayer:1jb45aa8ynh2a9c9xn3b7qqh8sm5q93hwfp7jqmwsfhh8jpz09h6" }
+
+from genlayer import *
+
+
+# contract class
+class Storage(gl.Contract):
+    storage: str
+
+    # constructor
+    def __init__(self, initial_storage: str):
+        self.storage = initial_storage
+
+    # read methods must be annotated with view
+    @gl.public.view
+    def get_storage(self) -> str:
+        return self.storage
+
+    # write method
+    @gl.public.write
+    def update_storage(self, new_storage: str) -> None:
+        self.storage = new_storageî
